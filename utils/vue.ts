f50f2c64e1c652b49aaf819a13d2f5ae3223ac68
Wuchieh <q20001116@gmail.com>
2024-12-23 14:49:30 +0800

    feat: Add the `useResetRef` method.

---
+++ utils/vue.ts
@@ -0,0 +1,19 @@
+import { cloneDeep } from 'lodash'
+/**
+ * @example
+ * const [num,resetNum] = useResetRef(0)
+ * console.log(num.value) // 0
+ * num.value += 10
+ * console.log(num.value) // 10
+ * resetNum()
+ * console.log(num.value) // 0
+ * */
+const useResetRef = <T = any>(value: T) => {
+  const valueRef = ref(cloneDeep(value))
+  const resetRef = () => {
+    valueRef.value = cloneDeep(value)
+  }
+  return [valueRef, resetRef] as const
+}
+
+export {useResetRef}
